3bb6dfc7f9fa946bd82e95ab355b95488586d477
Thaistranf <anhnamduong9x9@gmail.com>
2023-08-06 17:07:35 +0700

    add exercise

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        double f = 20.5;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+
+        System.out.printf("i = %d", i);
+        System.out.println("\nf = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
